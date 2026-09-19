996c8b19bd53e2436faa592da63be748a3cb5694
Jason Kumpf <jason.kumpf@apexsupplychain.com>
2026-09-19 19:41:28 -0400

    fixed forge.config.ts for linux builds

--- forge.config.ts
+++ forge.config.ts
@@ -14,6 +14,7 @@ const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
     icon: customIconPath,
+    executableName: 'kube-cluster-ui',
     // osxSign: {
     //   identity: process.env.APPLE_SIGNING_IDENTITY,
     //   hardenedRuntime: true,

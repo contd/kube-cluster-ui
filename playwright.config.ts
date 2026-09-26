bc1f53ab9bd3fedb21f49ba5a9bef8d9366db2b0
Jason Kumpf <jason.kumpf@apexsupplychain.com>
2026-09-25 21:14:51 -0400

    patch: added to workflow the html test reports added to project wiki

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
-  reporter: process.env.CI ? 'github' : 'list',
+  reporter: process.env.CI
+    ? [
+        ['github'],
+        ['html', { outputFolder: 'reports/e2e', open: 'never' }],
+      ]
+    : 'list',
   use: {
     baseURL: 'http://127.0.0.1:4173',
     trace: 'on-first-retry',
